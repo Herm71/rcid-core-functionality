6315e64d7ed125a05628df52111673e0bf0418cc
Jason Chafin <Herm71@users.noreply.github.com>
2020-10-28 14:31:46 -0700

    refactor: :art: Add footer sidebars

--- lib/functions/side-bars.php
+++ lib/functions/side-bars.php
@@ -2,13 +2,56 @@
  /**
  * Top bar sidebar.
  */
-add_action( 'widgets_init', 'rcid_topbar_widget' );
-function rcid_topbar_widget() {
+add_action( 'widgets_init', 'rcid_topbar_sidebar' );
+function rcid_topbar_sidebar() {
     register_sidebar( array(
         'name'          => __( 'Topbar', 'ruth-chafin-interior-design' ),
         'id'            => 'top-sidebar',
         'class'         => 'top-sidebar',
         'description'   => __( 'Widgets in this area will be shown in the Top bar.', 'ruth-chafin-interior-design' ),
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => ''
+    ) );
+}
+
+/**
+ * Footer sidebars.
+ */
+add_action( 'widgets_init', 'rcid_footer_one_widget' );
+function rcid_footer_one_widget() {
+    register_sidebar( array(
+        'name'          => __( 'Footer One', 'ruth-chafin-interior-design' ),
+        'id'            => 'footer-one',
+        'class'         => 'footer-one',
+        'description'   => __( 'Widgets in this area will be shown in footer.', 'ruth-chafin-interior-design' ),
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => ''
+    ) );
+}
+add_action( 'widgets_init', 'rcid_footer_three_widget' );
+function rcid_footer_three_widget() {
+    register_sidebar( array(
+        'name'          => __( 'Footer Three', 'ruth-chafin-interior-design' ),
+        'id'            => 'footer-three',
+        'class'         => 'footer-three',
+        'description'   => __( 'Widgets in this area will be shown in footer.', 'ruth-chafin-interior-design' ),
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => ''
+    ) );
+}
+add_action( 'widgets_init', 'rcid_footer_three_widget' );
+function rcid_footer_three_widget() {
+    register_sidebar( array(
+        'name'          => __( 'Footer Three', 'ruth-chafin-interior-design' ),
+        'id'            => 'footer-three',
+        'class'         => 'footer-three',
+        'description'   => __( 'Widgets in this area will be shown in footer.', 'ruth-chafin-interior-design' ),
         'before_widget' => '',
         'after_widget'  => '',
         'before_title'  => '',
