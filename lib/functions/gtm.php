84a86b69a4316ec9b923be086e88299e0dc86326
Jason Chafin <jchafin@ucsc.edu>
2024-01-09 21:13:40 -0800

    feat: :sparkles: Add Google Tag Manager in addition to Google Analytics

--- lib/functions/gtm.php
+++ lib/functions/gtm.php
@@ -12,9 +12,12 @@
  * @license   http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
 
-add_action('wp_body_open', 'rcid_google_tag_manager');
+add_action('wp_head', 'rcid_google_tag_manager_head' );
+add_action('wp_body_open', 'rcid_google_analytics' );
+add_action('wp_body_open', 'rcid_google_tag_manager_body' );
 
-function rcid_google_tag_manager()
+
+function rcid_google_analytics()
 {
     ?>
     <!-- Google tag (gtag.js) -->
@@ -26,6 +29,30 @@ function rcid_google_tag_manager()
 
       gtag('config', 'G-BLMEKZLMMC');
     </script>
+    <?php
+}
+
+function rcid_google_tag_manager_head()
+{
+    ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5K5NV59S');</script>
+    <!-- End Google Tag Manager -->
+    <?php
+
+}
+
+function rcid_google_tag_manager_body()
+{
+    ?>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5K5NV59S"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <?php
 }
 
